84e221265de031b6ee7c71de479b444f1bb226c9
eddieburke <eddiesgotmail@yahoo.com>
2025-12-18 22:25:04 -0500

    Update Fish.cpp

--- Fish.cpp
+++ Fish.cpp
@@ -1,42 +1,48 @@
 #include "Fish.h"
 #include "Board.h"
+#include <algorithm>
 
-using namespace Sexy;
+namespace Sexy {
 
-Fish::Fish(const std::string& name) 
-    : mName(name)
-    , mVariant(0)
-    , mIsSingingFish(false)
-    , mType(-1)
-    , mHasCostume(false)
-    , mIsSpecial(false)
-    , mX(0), mY(0)
-    , mColor1(255, 165, 0) // Default Orange
-    , mColor2(255, 255, 255)
-    , mColor3(0, 0, 0)
-{
+Fish::Fish(const std::string& theName) : mName(theName) {
+    mVariant = 0; mIsSinging = false; mType = -1;
+    mIsSpecial = false; mHasCostume = false;
 }
 
-Fish::~Fish() {}
+// Corresponds to FUN_0054cc70
+void Fish::CheckSpecialLogic(Board* theBoard, Fish* theFish) {
+    // 1. Check for Singing Fish ID
+    if (_stricmp(theFish->mName.c_str(), "1SingingFish") == 0) {
+        theFish->mIsSinging = true; // Offset 0x110
+        return;
+    }
 
-void Fish::Update(Board* theBoard)
-{
-    // Basic swim movement
-    mX += 1.0f;
-    if (mX > 800) mX = 0;
+    // 2. Check for Santa
+    if (_stricmp(theFish->mName.c_str(), "santa") == 0 && theFish->mVariant == 0) {
+        // Iterate board's fish list to see if Santa (Type 5) already exists
+        bool aSantaFound = false;
+        for (size_t i = 0; i < theBoard->mFishList.size(); ++i) {
+            if (theBoard->mFishList[i]->mType == 5) {
+                aSantaFound = true;
+                break;
+            }
+        }
+
+        if (aSantaFound) return;
+
+        // Transformation Logic
+        if (!theFish->mHasCostume && theFish->mType == -1) {
+            theFish->mType = 5;         // Type 5 = Santa
+            theFish->mHasCostume = true; // 0x1D0
+            theFish->mIsSinging = true;  // 0x110
+            theFish->mIsSpecial = true;  // 0x1A8
+            
+            // Set Santa Colors (Matches FUN_00433320 calls)
+            theFish->mColor1 = 0xFFFFFF; // White
+            theFish->mColor2 = 0xFF0000; // Red
+            theFish->mColor3 = 0xFFFFFF; // White
+        }
+    }
 }
 
-void Fish::Draw(Graphics* g)
-{
-    // Simple debug drawing
-    g->SetColor(mColor1);
-    g->FillRect((int)mX, (int)mY, 40, 30);
-    
-    // Draw Name/Type
-    g->SetColor(Color::White);
-    std::string label = mName;
-    if (mType == 5) label += " (SANTA)";
-    if (mIsSingingFish) label += " [Singing]";
-    
-    g->DrawString(label, (int)mX, (int)mY - 5);
 }
